f4d3d70cf8fe74268a7390dfc5e5bddd2bb3218c
falirezah <al.r.falah@gmail.com>
2025-03-16 16:27:06 +0100

    cleaned after the merge

--- signal_processing/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from utils.smoothing import *
-
-data = "../data/drill_broken.csv"
-# degree = data['Degree']
-# pixel_values = data['Sum of Pixels']
-
-smoothed_pixels = savgol_peak_finder(data)
-# smoothed_pixels = smoothing_moving_average(degree, pixel_values, 5)
