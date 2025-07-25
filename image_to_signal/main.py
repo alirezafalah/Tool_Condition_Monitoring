f36b00e3efabb4d02e35f22adfa3538264181cad
falirezah <al.r.falah@gmail.com>
2025-07-25 06:14:24 +0200

    included Lab

--- image_to_signal/main.py
+++ image_to_signal/main.py
@@ -25,11 +25,24 @@ CONFIG = {
 
     # --- Image Processing Parameters ---
     'blur_kernel': 13,
+    'closing_kernel': 5,
+
+    # -- HSV Parameters --
     'hue_min': 142,        # Corresponds to 200 degrees
     'hue_max': 163,        # Corresponds to 230 degrees
     'sat_min': 38,         # Corresponds to 15%
-    'lab_ab_max': 126,       # Corresponds to a/b < -2 (currently unused)
-    'closing_kernel': 5,
+    'sat_max': 255,       # Corresponds to 100%
+    'value_min': 0,        # Corresponds to 0%
+    'value_max': 255,      # Corresponds to 100%
+
+    # -- LAB Parameters --
+    'lab_bg_l_min': 128,     # Corresponds to L* = 50  (50 * 2.55)
+    'lab_bg_l_max': 133,     # Corresponds to L* = 52  (52 * 2.55)
+    'lab_bg_a_min': 126,     # Corresponds to a* = -2  (-2 + 128)
+    'lab_bg_a_max': 255,     # Corresponds to a* = 127 (max value)
+    'lab_bg_b_min': 0,       # Corresponds to b* = -128 (min value)
+    'lab_bg_b_max': 130,     # Corresponds to b* = 2   (2 + 128)
+
 
     # --- Data Analysis Parameters ---
     'images_for_366_deg': 372, # Because we used 5 Rev/min and we recorded for 12.2 seconds, therefore we have 366 degrees for each tool.
